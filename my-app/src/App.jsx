04fa17828b4daeaea40be5ebf4ab7e4197913741
RazeYogeshOO <yogeshdevkota60@gmail.com>
2026-06-03 21:51:08 +0545

    safe commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faLocationCrosshairs } from '@fortawesome/free-solid-svg-icons'
@@ -6,13 +7,32 @@ import wtwt from './images/wtwt.png'
 import bg1 from './images/bg1.jpg'
 
 function App() {
+  const [isStarted, setIsStarted] = useState(false);
+  const handleGetStarted = () => {
+    setIsStarted(true);
+  }
+
   return (
     <div className="App">
       <div className="container">
-        <img src={bg1} alt="bg1" width="100%" height="100%" position="absolute"/>
+        <img src={bg1} alt="bg1" />
         <div className="box1">
-          {/* <FontAwesomeIcon icon="fa-solid fa-location-crosshairs" /> */}
-          <img src={wtwt} alt="wtwt" width="430px" height="150px" />
+          {!isStarted ? (
+            <div className="wtwt-section">
+              <img src={wtwt} alt="wtwt" width="430px" height="150px" />
+              <button className="btn" onClick={handleGetStarted}>
+                <b>Get Started</b>
+              </button>
+            </div>
+          ) : (
+            <div className="search-section">
+              <input
+                type="text"
+                className="location-search"
+                placeholder="Enter your location"
+              />
+            </div>
+          )}
         </div>
       </div>
     </div>
