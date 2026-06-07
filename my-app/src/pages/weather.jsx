aa9281bcfd25cac4bf3e6862923cc77fa0168b49
RazeYogeshOO <yogeshdevkota60@gmail.com>
2026-06-07 10:25:29 +0545

    tiny css changes

--- my-app/src/pages/weather.jsx
+++ my-app/src/pages/weather.jsx
@@ -1,7 +1,19 @@
+import React from 'react';
+import './Weather.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+
 function Weather() {
   return (
-    <div>
-      <h1>Weather Page</h1>
+    <div className="weatherApp">
+          <div className="weatherContainer">
+            <div className="toppart">
+              <FontAwesomeIcon icon={faLocationDot} />
+              <p className="location"></p>
+            </div>
+            <div className="box1">
+             </div>
+        </div>
     </div>
   )
 }
